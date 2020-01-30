f895f048cd08ecb3350aa497329eeb7d92e9e77a
eoinlynchcodes <eoin95lynch@gmail.com>
2020-01-30 14:17:23 +0000

    Form complete with standard useState not Formik.

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -10,26 +10,53 @@ function Form(props) {
         termsOfService: false,
     });
 
+    const handleChanges = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(formData);
+    }
+
   return (
-    <form>
+    <form onSubmit={event => handleSubmit(event)}>
       <label>Name:</label>
       <input 
       type="text" 
-      placeholder="Name:" />
+      placeholder="Name:"
+      name="name"
+      value={formData.name}
+      onChange={event => handleChanges(event)}
+      />
 
       <label>Email:</label>
       <input 
       type="email" 
-      placeholder="Email:" />
+      placeholder="Email:" 
+      name="email"
+      value={formData.email}
+      onChange={event => handleChanges(event)}
+      />
 
       <label>Password</label>
       <input 
       type="password" 
-      placeholder="password" />
+      placeholder="password"
+      name="password"
+      value={formData.password}
+      onChange={event => handleChanges(event)}
+      />
 
       <label>Terms of Service:</label>
       <input 
-      type="checkbox" />
+      type="checkbox"
+      name="termsOfService"
+      value={formData.termsOfService}
+      />
 
       <button>Submit!</button>
     </form>
